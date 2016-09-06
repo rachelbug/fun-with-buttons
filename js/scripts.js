d77ed7f7a749ae8bc7684e816cff9ab982bcca93
Rachel Etnire <rachel.etnire@gmail.com>
2016-09-05 19:57:18 -0700

    add js and button that changes page back to dark on light

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,13 @@
 $(function(){
   $("#night").click(function() {
+    $("body").removeClass();
     $("body").addClass("darkness")
+  });
+});
+
+$(function(){
+  $("#light").click(function(){
+    $("body").removeClass();
+    $("body").addClass("lightness")
   });
 });
