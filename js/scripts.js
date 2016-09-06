5701e7496c030d6064c1aa67b96412f7784b275a
Rachel Etnire <rachel.etnire@gmail.com>
2016-09-06 08:38:29 -0700

    Add functionality so when you click on the paragraph below the buttons a dotted border appears and the text is highlighted

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 $(function(){
-  $("#night").click(function() {
+  $("#night").click(function(){
     $("body").removeClass();
-    $("body").addClass("darkness")
+    $("body").addClass("darkness");
   });
 });
 
 $(function(){
   $("#light").click(function(){
     $("body").removeClass();
-    $("body").addClass("lightness")
+    $("body").addClass("lightness");
+  });
+});
+
+$(function(){
+  $("#clickable").click(function(){
+    $(".borderfun").addClass("new-border");
   });
 });
